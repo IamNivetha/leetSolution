008481edb47e09dc4e91b88c3d6f4f19bbc70e5c
IamNivetha <dhanyukanivetha@gmail.com>
2026-05-26 11:58:14 +0530

    Rotate Array by k

---
+++ Rotate.java
@@ -0,0 +1,27 @@
+public class Rotate {
+    static void rotateArr(int arr[], int d) {
+        // code here
+        int n = arr.length;
+        d = d % n;
+        
+        reverse(arr,0,d - 1);
+        reverse(arr,d,n - 1);
+        reverse(arr,0,n - 1);
+        
+        
+        
+    }
+    static void reverse(int [] arr,int left,int right)
+    {
+        
+        while(left<right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+        }
+        
+        
+    }
+} 
